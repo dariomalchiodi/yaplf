0278772a3aae4ad275e64f3a3afcfc8bea317ddb
Dario Malchiodi <malchiodi@di.unimi.it>
2013-01-08 00:22:31 +0100

    Added copyright notes.

--- yaplf/graph/trajectory.py
+++ yaplf/graph/trajectory.py
@@ -17,8 +17,18 @@ AUTHORS:
 #*****************************************************************************
 #       Copyright (C) 2010 Dario Malchiodi <malchiodi@dsi.unimi.it>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#                  http://www.gnu.org/licenses/
+# This file is part of yaplf.
+# yaplf is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# yaplf is distributed in the hope that it will be useful, but without any
+# warranty; without even the implied warranty of merchantability or fitness
+# for a particular purpose. See the GNU Lesser General Public License for
+# more details.
+# You should have received a copy of the GNU Lesser General Public License
+# along with yaplf; if not, see <http://www.gnu.org/licenses/>.
+#
 #*****************************************************************************
 
 
